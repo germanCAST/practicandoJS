87a4c01fb178d72ab66e400d892b4c4224e5c452
German Castellanos <87241711+germanCAST@users.noreply.github.com>
2024-02-14 15:55:08 -0600

    Delete 12-herencia directory

--- 12-herencia/05-comp-vs-her.js
+++
@@ -1,8 +0,0 @@
-//vuela, nada, camina, va al baño
-function Pato() {}
-//camina,nada, va al baño
-function Perro() {}
-//nada, va al baño
-function Pez() {}
-//vuela pero no nada, camina, ni va al bañ
-function Avion() {}
